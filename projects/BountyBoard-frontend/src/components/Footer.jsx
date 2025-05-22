3503ae534c87b3552576ee04a1d255319cd61c42
abhisathvik <abhisathvikreddy1738527@gmail.com>
2025-05-22 19:19:06 +0530

    Create Footer.jsx

---
+++ projects/BountyBoard-frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/Footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>© 2025 Algorand Community. All rights reserved.</p>
+        </footer>
+    );
+}

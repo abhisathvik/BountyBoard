2dc3157986337b20a6bd74e6006dad3202baa3cf
abhisathvik <abhisathvikreddy1738527@gmail.com>
2025-05-22 19:05:33 +0530

    Create LoginPage.jsx

---
+++ projects/BountyBoard-frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../components/Footer.jsx'
+import Header from '../components/Header.jsx'
+import Login from '../components/Login.jsx'
+
+export default function LoginPage() {
+  return (
+    <div id="login">
+      <Header />
+      <Login />
+      <Footer />
+    </div>
+      
+  )
+}

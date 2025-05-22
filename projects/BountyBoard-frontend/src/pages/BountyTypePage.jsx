d9587cb8f5a1f27b853684306464af3a1ffda703
abhisathvik <abhisathvikreddy1738527@gmail.com>
2025-05-22 19:04:28 +0530

    Create BountyTypePage.jsx

---
+++ projects/BountyBoard-frontend/src/pages/BountyTypePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from '../components/Footer.jsx'
+import Header from '../components/Header.jsx'
+import BountyType from '../components/BountyType.jsx'
+import Navbar from '../components/NavBar.jsx'
+import '../css/Register.css'
+
+export default function BountyTypePage() {
+  return (
+    <div className='bountyType'>
+      <Header />
+      <Navbar />
+      <Footer />
+    </div>
+      
+  )
+}

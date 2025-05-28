feb1226c3d212f5908e80551044e45f1478ddf49
Aniga Abhi Sathvik Reddy <151656430+abhisathvik@users.noreply.github.com>
2025-05-28 12:08:38 +0530

    Integrated Pera Wallet

--- projects/BountyBoard-frontend/src/App.jsx
+++ projects/BountyBoard-frontend/src/App.jsx
@@ -35,17 +35,30 @@
 // export default App
 
 import React from 'react';
-import RegisterPage from './pages/RegisterPage.jsx'
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
+import { NetworkId, WalletId, WalletManager, WalletProvider } from '@txnlab/use-wallet-react';
 
 
 
 export default function App() {
-  return (
-    <Router>
-      <AppRoutes />
-    </Router>
+  const walletManager = new WalletManager({
+  wallets: [
+    WalletId.PERA,
+  ],
+  defaultNetwork: NetworkId.TESTNET,
+  dappMetadata: {
+    name: 'Algo Bounty Board'
+  }
+  })
 
-  )
+  return (
+    <WalletProvider
+      manager={walletManager}
+    >
+      <Router>
+        <AppRoutes />
+      </Router>
+    </WalletProvider>
+  );
 }
